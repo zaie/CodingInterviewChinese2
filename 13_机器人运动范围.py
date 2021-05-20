754ffdc29d1d85517134035e9f85eec9c3740d24
zaie <948064756@qq.com>
2021-05-20 10:22:55 +0800

    Update 13_机器人运动范围.py

--- 13_机器人运动范围.py
+++ 13_机器人运动范围.py
@@ -1,42 +1,31 @@
-def moving_count(matrix, k):
-    if not isinstance(matrix, list) or len(matrix) == 0 or \
-            not isinstance(matrix[0], list) or len(matrix[0]) == 0:
-        return False
+# 方法一：深度优先遍历 DFS
+# 深度优先搜索： 可以理解为暴力法模拟机器人在矩阵中的所有路径。DFS 通过递归，先朝一个方向搜到底，再回溯至上个节点，沿另一个方向搜索，以此类推。
+# 剪枝： 在搜索中，遇到数位和超出目标值、此元素已访问，则应立即返回，称之为 可行性剪枝 。
+# 算法解析：
+# 递归参数： 当前元素在矩阵中的行列索引 i 和 j ，两者的数位和 si, sj 。
+# 终止条件： 当 ① 行列索引越界 或 ② 数位和超出目标值 k 或 ③ 当前元素已访问过 时，返回 00 ，代表不计入可达解。
+# 递推工作：
+# 标记当前单元格 ：将索引 (i, j) 存入 Set visited 中，代表此单元格已被访问过。
+# 搜索下一单元格： 计算当前元素的 下、右 两个方向元素的数位和，并开启下层递归 。
+# 回溯返回值： 返回 1 + 右方搜索的可达解总数 + 下方搜索的可达解总数，代表从本单元格递归搜索的可达解总数。
 
-    rows = len(matrix)
-    cols = len(matrix[0])
-    for r in matrix:
-        if len(r) != cols:
-            return False
+# 复杂度分析：
+# 设矩阵行列数分别为 M, NM,N 。
 
-    visited = [[False] * cols for _ in range(rows)]
-    count = count_recursively(matrix, k, (0, 0), visited)
-    return count
+# 时间复杂度 O(MN) ： 最差情况下，机器人遍历矩阵所有单元格，此时时间复杂度为 O(MN) 。
+# 空间复杂度 O(MN) ： 最差情况下，Set visited 内存储矩阵所有单元格的索引，使用 O(MN) 的额外空间
 
+class Solution:
+    def movingCount(self, m: int, n: int, k: int) -> int:
+        def dfs(i, j, si, sj):
+            if i >= m or j >= n or k < si + sj or (i, j) in visited: return 0
+            visited.add((i,j))
+            return 1 + dfs(i + 1, j, si + 1 if (i + 1) % 10 else si - 8, sj) + dfs(i, j + 1, si, sj + 1 if (j + 1) % 10 else sj - 8)
 
-def count_recursively(matrix, k, matrix_indices, visited):
-    count = 0
-    m_i, m_j = matrix_indices
-    if check(matrix, k, matrix_indices, visited):
-        count = count + count_recursively(matrix, k, (m_i + 1, m_j), visited) + \
-            count_recursively(matrix, k, (m_i - 1, m_j), visited) + \
-            count_recursively(matrix, k, (m_i, m_j + 1), visited) + \
-            count_recursively(matrix, k, (m_i, m_j - 1), visited)
-    return count
+        visited = set()
+        return dfs(0, 0, 0, 0)
 
-
-def check(matrix, k , matrix_indices, visited):
-    m_i, m_j = matrix_indices
-    if not (0 <= m_i < len(matrix)) or not (0 <= m_j < len(matrix[0])) or \
-            (sum_digit(m_i) + sum_digit(m_j) > k) or \
-            visited[m_i][m_j]:
-        return False
-    return True
-
-
-def sum_digit(x):
-    sum = 0
-    while x > 0:
-        sum += x % 10
-        x = x // 10
-    return sum
+# 作者：jyd
+# 链接：https://leetcode-cn.com/problems/ji-qi-ren-de-yun-dong-fan-wei-lcof/solution/mian-shi-ti-13-ji-qi-ren-de-yun-dong-fan-wei-dfs-b/
+# 来源：力扣（LeetCode）
+# 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
